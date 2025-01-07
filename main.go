869107b0084f34e3dbe8b77b9a7339050d35cb02
yusrilaprial <myusrila23@gmail.com>
2025-01-07 16:09:20 +0700

    feat: connect db with gorm

--- main.go
+++ main.go
@@ -5,18 +5,14 @@ import (
 )
 
 func main() {
-	//inisialiasai Gin
 	router := gin.Default()
 
-	//membuat route dengan method GET
 	router.GET("/", func(c *gin.Context) {
 
-		//return response JSON
 		c.JSON(200, gin.H{
 			"message": "Hello World!",
 		})
 	})
 
-	//mulai server dengan port 3000
 	router.Run(":3000")
 }
